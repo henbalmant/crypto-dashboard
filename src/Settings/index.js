97cf4883b8c2f873979f2c9464d147963e27de2f
henbalmant <henrique.balmant@gmail.com>
2020-06-24 16:43:10 -0300

    Add Page component

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import ConfirmButton from './ConfirmButton';
+import Page from '../Shared/Page';
 
 const Settings = () => {
-  return <div> <WelcomeMessage/> <ConfirmButton/> </div>
+  return(
+    <Page name='settings'>
+      <WelcomeMessage/>
+      <ConfirmButton/>
+    </Page>
+  );
 }
 
 export default Settings;
